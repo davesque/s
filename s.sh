a8d6dc85f813090de70b5b7cac5749a56afd930e
David Sanders <dsanders@fusionbox.com>
2013-09-18 18:03:15 -0600

    First version of s

--- s.sh
+++ s.sh
@@ -1,189 +1,179 @@
-#!/usr/bin/env bash
-
 # Switch board
-case $1 in 
-  "-l"|"--list")
-    __f_list
-    ;;
-  "-h"|"--help")
-    __f_help
-    ;;
-  "-i"|"--init")
-    __f_init -i
-    ;;
-  "-m"|"--move")
-    __f_move $2 $3
-    ;;
-  "-c"|"--copy")
-    __f_copy $2 $3
-    ;;
-  "-d"|"--delete")
-    __f_delete $2
-    ;;
-  *)
-    if [[ -z $1 ]]; then
-      __f_list
-    else
-      __f_edit $1
-    fi
-    ;;
-esac
-
-# Initializes zf
-function __f_init() {
-  # Ensure zf_path is set
-  if [[ -z $zf_path ]]; then
-    print "Must set zf_path environment variable before loading zf" >& 2
+function s {
+  if [[ -z $S_INITIALIZED ]]; then
+    cat <<ERROR
+\`s\` did not initialize properly.  Please open a new shell and inspect any error
+messages which are displayed.
+ERROR
     return 1
   fi
 
+  case $1 in
+    "-l"|"--list")
+      __s_list
+      ;;
+    "-h"|"--help")
+      __s_help
+      ;;
+    "-m"|"--move")
+      __s_move $2 $3
+      ;;
+    "-c"|"--copy")
+      __s_copy $2 $3
+      ;;
+    "-d"|"--delete")
+      __s_delete $2
+      ;;
+    *)
+      if [[ -z $1 ]]; then
+        __s_list
+      else
+        __s_edit $1
+      fi
+      ;;
+  esac
+}
+
+# Initializes s
+function __s_init {
   # Ensure EDITOR is set
   if [[ -z $EDITOR ]]; then
-    print "Must set EDITOR environment variable before loading zf" >& 2
+    echo "Must set EDITOR environment variable to use s" >& 2
     return 1
   fi
 
-  # Set default functions path
-  if [[ -z $zf_unctions_path ]]; then
-    zf_unctions_path="$zf_path/unctions"
+  # Set default bin path
+  if [[ -z $S_BIN_PATH ]]; then
+    export S_BIN_PATH="$HOME/.bin"
   fi
 
-  # Add $zf_unctions_path to fpath
-  if [[ $fpath[(r)$zf_unctions_path] != $zf_unctions_path ]]; then
-    fpath=($zf_unctions_path $fpath)
+  # Ensure $S_BIN_PATH is in PATH
+  echo "$PATH" | grep "$S_BIN_PATH" &> /dev/null
+  if [[ $? -eq 1 ]]; then
+    export PATH=$S_BIN_PATH:$PATH
   fi
 
-  if [[ $1 == "-i" ]]; then
-    print "Restarting zf..."
+  export S_INITIALIZED="done"
+}
 
-    # Unload functions in $zf_unctions_path
-    for func in $(ls $zf_unctions_path); do
-      unfunction $func &> /dev/null
-    done
+# Edits or adds a script in $S_BIN_PATH
+function __s_edit {
+  [[ -z $1 ]] && return 1
 
-    # Reload zf
-    unfunction f && autoload -U f && f
+  local s_loc=$S_BIN_PATH/$1
+
+  if [[ ! -e $s_loc ]]; then
+    echo "#!/usr/bin/env bash" >> $s_loc
+    chmod 755 $s_loc
+  fi
+
+  if [[ -n $S_EDITOR_ARGS ]]; then
+    eval $EDITOR $S_EDITOR_ARGS $s_loc
   else
-    # Set up all functions in $zf_unctions_path to be autoloaded
-    for func in $(ls $zf_unctions_path); do
-      autoload -U $func
-    done
-
-    # If functions have been marked as requiring initialization...
-    if [[ -n $zf_init ]]; then
-      for func in $zf_init; do
-        if [[ -r $zf_unctions_path/$func ]]; then
-          $func
-        else
-          print "Tried to initialize $func, but couldn't find or read it" >& 2
-        fi
-      done
-    fi
+    $EDITOR $s_loc
   fi
 }
 
-# Edits or adds a function in $zf_unctions_path
-function __f_edit {
-  if [[ -n $1 ]]; then
-    if [[ -n $zf_editor_args ]]; then
-      eval $EDITOR $zf_editor_args $zf_unctions_path/$1
-    else
-      $EDITOR $zf_unctions_path/$1
-    fi
+# Renames a function in $S_BIN_PATH
+function __s_move {
+  [[ -z $1 || -z $2 ]] && return 1
 
-    __f_init -i
+  local s_old=$S_BIN_PATH/$1
+  local s_new=$S_BIN_PATH/$2
+
+  if [[ -e $s_old && ! -e $s_new ]]; then
+    echo "Renaming $1 to $2..."
+    mv $s_old $s_new
+  elif [[ ! -e $s_old ]]; then
+    echo "$1 not found!" >& 2
+  elif [[ -e $s_new ]]; then
+    echo "$2 already exists!" >& 2
+  else
+    # Should never happen
+    return 1
   fi
 }
 
-# Renames a function in $zf_unctions_path
-function __f_move {
-  if [[ -n $1 && -n $2 ]]; then
-    local f_old=$zf_unctions_path/$1
-    local f_new=$zf_unctions_path/$2
+# Copies a function in $S_BIN_PATH
+function __s_copy {
+  [[ -z $1 || -z $2 ]] && return 1
 
-    if [[ -e $f_old && ! -e $f_new ]]; then
-      print "Renaming $1 to $2..."
-      unfunction $1
-      mv $f_old $f_new
-      autoload -U $2
-    elif [[ ! -e $f_old ]]; then
-      print "$1 not found!" >& 2
-    elif [[ -e $f_new ]]; then
-      print "$2 already exists!" >& 2
-    else
-      # Should never happen
-    fi
+  local s_old=$S_BIN_PATH/$1
+  local s_new=$S_BIN_PATH/$2
+
+  if [[ -e $s_old && ! -e $s_new ]]; then
+    echo "Copying $1 to $2..."
+    cp $s_old $s_new
+  elif [[ ! -e $s_old ]]; then
+    echo "$1 not found!" >& 2
+  elif [[ -e $s_new ]]; then
+    echo "$2 already exists!" >& 2
+  else
+    # Should never happen
+    return 1
   fi
 }
 
-# Copies a function in $zf_unctions_path
-function __f_copy {
-  if [[ -n $1 && -n $2 ]]; then
-    local f_old=$zf_unctions_path/$1
-    local f_new=$zf_unctions_path/$2
+# Removes a function in $S_BIN_PATH
+function __s_delete {
+  [[ -z $1 ]] && return 1
 
-    if [[ -e $f_old && ! -e $f_new ]]; then
-      print "Copying $1 to $2..."
-      unfunction $1
-      cp $f_old $f_new
-      autoload -U $2
-    elif [[ ! -e $f_old ]]; then
-      print "$1 not found!" >& 2
-    elif [[ -e $f_new ]]; then
-      print "$2 already exists!" >& 2
-    else
-      # Should never happen
-    fi
+  local s_loc=$S_BIN_PATH/$1
+
+  if [[ -e $s_loc ]]; then
+    echo "Deleting $1..."
+    rm $s_loc
+  else
+    echo "$1 not found!" >& 2
   fi
 }
 
-# Removes a function in $zf_unctions_path
-function __f_delete {
-  if [[ -n $1 ]]; then
-    local f_loc=$zf_unctions_path/$1
-
-    if [[ -e $f_loc ]]; then
-      print "Deleting $1..."
-      unfunction $1
-      rm $f_loc
-    else
-      print "$1 not found!" >& 2
-    fi
-  fi
+# Lists all functions in $S_BIN_PATH
+function __s_list {
+  echo "${fg_bold[yellow]}List of scripts:${reset_color}"
+  ls -1 $S_BIN_PATH
 }
 
-# Lists all functions in $zf_unctions_path
-function __f_list {
-  print "${fg_bold[yellow]}List of functions:${reset_color}"
-  ls -1 $zf_unctions_path
-}
-
-function __f_help {
+function __s_help {
   cat <<HELP
-zf, a simple function tracking system for zshell
+s, a simple shell script manager
 
-usage: f [options] [function]
+usage: s [options] [script]
 
-Thanks goes to Colin Thomas-Arnold for writing the original f function tracking
-system for bash (https://github.com/colinta/f).  zf is known as 'f' on the
-command line for the purpose of abbreviation.
+\`s\` is hosted on github at \`https://github.com/davesque/s\`.  It was
+originally inspired by \`f\` (https://github.com/colinta/f).
 
-optional arguments:
-  -l, --list          List all functions.
-                      Default command if no arguments are passed to zf.
-  -m, --move old new  Renames a function 'old' to 'new'.
-  -c, --copy old new  Copies a function 'old' to 'new'.
-  -d, --delete fn     Deletes the function 'fn'.
-  -i, --init          Reload all functions.
-  -h, --help          Show this help screen.
+Info/manipulation:
+  -l, --list            List all scripts.
+                        Default command if no arguments are passed.
+  -m, --move foo bar    Renames a script 'foo' to 'bar'.
+  -c, --copy foo bar    Copies a script 'foo' to 'bar'.
+  -d, --delete <foo>    Deletes the script 'foo'.
+
+Editing/creation:
+  -b, --bash <foo>      Edit/create bash script 'foo'.
+                        Default if a script name is given, but no script
+                        type is specified.
+  -z, --zsh <foo>       Edit/create zsh script 'foo'.
+  -p, --python <foo>    Edit/create python script 'foo'.
+  -r, --ruby <foo>      Edit/create ruby script 'foo'.
+  -pe, --perl <foo>     Edit/create perl script 'foo'.
+
+Etc:
+  -h, --help            Show this help screen.
 
 examples:
 
-To create a new function using zf called 'lo', issue the following command:
+To create a new script using \`s\` called \`lo\`, issue the following command:
 
-  % f lo
+  $ s lo
 
-This will open a new file using \$EDITOR.  Enter some code:
+This will open a new file using \`\$EDITOR\`.  By default, \`s\` will
+automatically insert a bash shebang line at the top.  This behavior can be
+adjusted with the \`-z\`, \`-p\`, \`-r\`, and \`-pe\` options.  Enter some
+code:
+
+  #!/usr/bin/env bash
 
   if [[ \$# -eq 0 ]]; then
     libreoffice --help
@@ -191,19 +181,20 @@ This will open a new file using \$EDITOR.  Enter some code:
     libreoffice \$@ &!
   fi
 
-Save and exit.  zf saves this code in the directory specified by
-\$zf_unctions_path, which has been made part of your zsh \$fpath.  It then
-marks it to be autoloaded.  Try out the new function:
+Save and exit.  \`s\` saves this code in the directory specified by
+\`\$S_BIN_PATH\`, which should be added to your binary search path.  Try out
+the new script:
 
-  % lo somefile.doc
+  $ lo somefile.doc
 
-What if you want to edit 'lo' later?...
+What if you want to edit \`lo\` later?...
 
-  % f lo
+  $ s lo
 
-...opens the code for 'lo' in \$EDITOR.  Make your changes, save, and quit.
+...opens the code for \`lo\` in \`\$EDITOR\`.  Make your changes, save, and
+quit.
 
 HELP
 }
 
-__f_init
+__s_init
