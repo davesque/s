e8814644ae90bfb4801813bd350e638dc07c0a8a
David Sanders <david@retul.com>
2014-08-22 17:18:38 -0600

    Simplify bash completion function

--- completion.sh
+++ completion.sh
@@ -1,16 +1,7 @@
 _s() {
-  COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-
-  if [[ $COMP_CWORD -eq 1 ]]; then
-    # Get full paths for shell scripts
-    opts=($S_BIN_PATH/*)
-
-    # Get file basenames
-    basenames="${opts[@]##*/}"
-
-    COMPREPLY=( $(compgen -W "${basenames[@]}" -- $cur) )
-  fi
+  opts=($S_BIN_PATH/*)
+  COMPREPLY=( $(compgen -W "${opts[*]##*/}" -- $cur) )
 }
 
 complete -F _s s
